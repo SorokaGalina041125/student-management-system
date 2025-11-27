e3ade966b37e0d7909a88f0a6077f323c9b1b5f3
Soroka Galina <galina.me@icloud.com>
2025-11-27 15:52:12 +0400

    feat: add class interactions (Task 2) - rate_hw and rate_lecture methods

--- student_management.py
+++ student_management.py
@@ -9,6 +9,17 @@ class Student:
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+        
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if not hasattr(lecturer, 'lecture_grades'):
+                lecturer.lecture_grades = {} 
+            if course in lecturer.lecture_grades:
+                lecturer.lecture_grades[course].append(grade)
+            else:
+                lecturer.lecture_grades[course] = [grade]
+        else:
+            return 'Error'
 
 
 class Mentor:
@@ -23,4 +34,11 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Error'
